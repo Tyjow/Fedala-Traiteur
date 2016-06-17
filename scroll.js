e480837b3dedde45577270580b5060cddb0402eb
Tyjow <platon.jonathan@gmail.com>
2016-06-17 16:22:35 +0200

    Firstcommit

---
+++ scroll.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#change');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").addClass('scroll');
+          $(".navbar-default .navbar-header .navbar-brand").addClass('scroll');
+          $(".navbar-default .navbar-header .navbar-brand:hover").addClass('scroll');
+          $(".navbar-default .nav > li > a").addClass('scroll');
+          $(".navbar-default .nav > li > a:hover").addClass('scroll');
+       } else {
+          $(".navbar-default").removeClass('scroll');
+          $(".navbar-default .navbar-header .navbar-brand").removeClass('scroll');
+          $(".navbar-default .navbar-header .navbar-brand:hover").removeClass('scroll');
+          $(".navbar-default .nav > li > a").removeClass('scroll');
+          $(".navbar-default .nav > li > a:hover").removeClass('scroll');
+       }
+   });
+    }
+});
+
+
+$(document).ready(function() {
+    var words = $('h1').text().split(' ');
+    var html = '';
+    $.each(words, function() {
+        html += '<span style="font-size:80px">'+this.substring(0,1)+'</span>'+this.substring(1) + ' ';
+    });
+    $('h1').html(html);
+});
+
+$(document).ready(function() {
+    var words = $('.navbar-brand').text().split(' ');
+    var html = '';
+    $.each(words, function() {
+        html += '<span style="font-size:45px">'+this.substring(0,1)+'</span>'+this.substring(1) + ' ';
+    });
+    $('.navbar-brand').html(html);
+});
