1ada621f6fcb07ee32f30b7679347c9b7362f724
Antoine Le Gonidec <vv221@dotslashplay.it>
2016-07-22 10:59:51 +0200

    evenement - amélioration de l’effet de hover

--- js/evenement.js
+++ js/evenement.js
@@ -1,15 +1,25 @@
 /* show text on hover */
-function showHover (hoverID, imgID) {
+function showHover (hoverID) {
 	var hover =  document.getElementById(hoverID);
+	var imgID = "img-" + hoverID;
 	var img = document.getElementById(imgID);
-	hover.style.opacity = 1;
+	var boxClass = "box-" + hoverID;
+	var box = document.getElementsByClassName(boxClass);
 	img.classList.add('black');
+	hover.style.opacity = 1;
+	box[0].style.opacity = 1;
+	box[1].style.opacity = 1;
 }
 
 /* hide text when leaving hover */
-function hideHover (hoverID, imgID) {
+function hideHover (hoverID) {
 	var hover =  document.getElementById(hoverID);
+	var imgID = "img-" + hoverID;
 	var img = document.getElementById(imgID);
-	hover.style.opacity = 0;
+	var boxClass = "box-" + hoverID;
+	var box = document.getElementsByClassName(boxClass);
 	img.classList.remove('black');
+	hover.style.opacity = 0;
+	box[0].style.opacity = 0;
+	box[1].style.opacity = 0;
 }
